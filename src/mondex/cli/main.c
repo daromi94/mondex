4765e3cb6b777092bc9f62a0ff91f6afb010c353
daromi94 <davromero07@gmail.com>
2024-08-27 20:41:49 -0500

    feat: :sparkles: check minimum argument count

--- src/mondex/cli/main.c
+++ src/mondex/cli/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <sysexits.h>
+
+#define PROGRAM "mondex"
+
+const int MINIMUM_ARGUMENT_COUNT = 2;
+
+void write_usage() {
+  printf("Usage: %s\n", PROGRAM);
+}
 
 int main(int argc, char **argv) {
-  printf("Hello, World!\n");
-  return 0;
+  if (argc < MINIMUM_ARGUMENT_COUNT) {
+    write_usage();
+    return EX_USAGE;
+  }
+
+  return EX_OK;
 }
